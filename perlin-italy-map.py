dc89330c5048b30d9bb21d9f84708b8fd02422f9
lorossi <lo.rossi@live.com>
2020-12-13 16:22:33 +0100

    small improvements

--- perlin-italy-map.py
+++ perlin-italy-map.py
@@ -5,9 +5,9 @@ import subprocess
 
 from pathlib import Path
 from math import pi, sin, cos
-from PIL import Image, ImageDraw
 from opensimplex import OpenSimplex
 from scipy.interpolate import interp1d
+from PIL import Image, ImageFont, ImageDraw
 
 
 # correct pixel range
@@ -68,21 +68,34 @@ def spline(x, y, length):
 
 
 class Map:
-    def __init__(self):
+    def __init__(self, destination_size):
         self.saturation = [15, 256]  # inside of this
         self.hue = [165, 240]  # outside of this
         # PARAMETERS
-        self.min_width = 70
-        self.y_resolution = 100
-        self.y_scl = 5
-        self.y_levels = 32
-        self.x_resolution = 50
-        self.x_levels = 32
+        # min width of a line
+        self.min_width = 100
+        # y spacing between lines
+        self.y_resolution = 40
+        # max dh of a line (relative to y uyresolution)
+        self.y_scl = 8
+        # y quantization in levels
+        self.y_levels = 16
+        # x spacing between lines
+        self.x_resolution = 40
+        # number of interpolation
+        self.x_levels = 16
+        # max x spacing between lines
         self.max_gap = 75
-        self.destination_size = 1200  # the size of the destination image and video
-        self.noise_scl = 5
-        self.noise_radius = 1
-
+        # pixel width of destination image
+        self.destination_size = destination_size
+        # noise parameters
+        self.noise_scl = 0.005
+        self.noise_radius = 2.5
+        # line alpha
+        self.line_alpha = 5
+        # watermark font size
+        self.font_size = 40
+        # noise initialization
         seed = int(time.time())
         self.noise = OpenSimplex(seed=seed)
 
@@ -91,11 +104,21 @@ class Map:
         self.hsv_im = self.source_im.convert("HSV")  # HSV - detect italy shape
         self.bw_im = self.source_im.convert("L")  # Black and White - height
 
+        # source image size
         image_width = self.source_im.width
         image_height = self.source_im.height
+        # biggest of the two
         biggest = max(image_width, image_height)
+        # scale of the destination image
         self.scl = self.destination_size / biggest
-        self.line_width = 4 / scl
+        # offset of the destination image
+        self.dx = int((self.destination_size - image_width * self.scl) / 2)
+        self.dy = int((self.destination_size - image_height * self.scl) / 2)
+        # calculate line width
+        self.line_width = int(1 / self.scl)
+
+    def loadFont(self, path):
+        self.font = ImageFont.truetype(font=path, size=self.font_size)
 
     def loadLines(self):
         # detect lines from image
@@ -110,10 +133,13 @@ class Map:
                 sat = pixel[1]  # saturation
                 hue = pixel[0]  # hue
 
-                if (self.saturation[0] < sat < self.saturation[1] and (hue < self.hue[0] or hue > self.hue[1])):
-                    # this is land, start line
-                    if not started:
-                        started = x
+                # check if saturation is in boundaries (to find Italy)
+                if (self.saturation[0] < sat < self.saturation[1]):
+                    # check if hue is outside boundaries (to find land)
+                    if (hue < self.hue[0] or hue > self.hue[1]):
+                        # this is land, start line
+                        if not started:
+                            started = x
 
                 elif started:
                     # if started, its time to end since we found sea
@@ -134,8 +160,12 @@ class Map:
 
         # fill gaps between lines
         for line in range(len(self.lines_coords) - 1):
-            if self.lines_coords[line]["start"]["y"] == self.lines_coords[line + 1]["start"]["y"]:  # on the same y
-                if self.lines_coords[line+1]["start"]["x"] - self.lines_coords[line]["end"]["x"] < self.max_gap:
+            y = self.lines_coords[line]["start"]["y"]
+            next_y = self.lines_coords[line + 1]["start"]["y"]
+            if y == next_y:  # on the same y
+                x = self.lines_coords[line]["start"]["x"]
+                next_x = self.lines_coords[line+1]["end"]["x"]
+                if next_x - x < self.max_gap:
                     self.lines_coords[line]["end"]["x"] = self.lines_coords[line+1]["start"]["x"] - self.x_resolution
 
     def calculateHeights(self):
@@ -149,9 +179,7 @@ class Map:
             for x in range(x_start, x_end, self.x_resolution):
                 heights = []
                 for dx in range(self.x_resolution):
-                    for y in range(y_start, y_start + self.y_resolution):
-                        if (x + dx < self.bw_im.width and y < self.bw_im.height):
-                            heights.append(self.bw_im.getpixel((x + dx, y)))
+                    heights.append(self.bw_im.getpixel((x + dx, y_start)))
 
                 avg_height = int(sum(heights) / len(heights))
 
@@ -159,11 +187,11 @@ class Map:
                 self.height_map.append({
                     "start": {
                         "x": x,
-                        "y": y
+                        "y": y_start
                     },
                     "end": {
                         "x": x + self.x_resolution,
-                        "y": y
+                        "y": y_start
                     },
                     "height": avg_height
                 })
@@ -181,9 +209,9 @@ class Map:
             n_height = map(height["height"], lowest, highest, 0, self.d_height)
             # quantize height
             q_height = quantize(n_height, self.y_levels, 0, self.d_height)
-            color = int(map(height["height"], lowest, highest, 0, 255))
             height["normalized_height"] = q_height
-
+            # get color (greyscale)
+            color = int(map(height["height"], lowest, highest, 0, 255))
             height["color"] = (color, color, color)
 
     def generateLines(self, percent):
@@ -195,25 +223,30 @@ class Map:
         nx = self.noise_radius * (1 + cos(ntheta))
         ny = self.noise_radius * (1 + sin(ntheta))
 
-        unique_y = list(set(height["start"]["y"] for height in self.height_map))  # list of y values
+        # list of unique y values
+        unique_y = list(set(h["start"]["y"] for h in self.height_map))
         for y in unique_y:
             line_x = []
             line_y = []
 
             lines_to_draw = [line for line in self.height_map if line["start"]["y"] == y]
             for line in range(len(lines_to_draw)):
-
+                # noise coordinates
+                nz = lines_to_draw[line]["start"]["x"] * self.noise_scl
+                nw = lines_to_draw[line]["start"]["y"] * self.noise_scl
                 # noise value
-                n = self.noise.noise4d(x=nx, y=ny, z=lines_to_draw[line]["start"]["x"] * self.noise_scl, w=lines_to_draw[line]["start"]["y"] * self.noise_scl)
+                n = self.noise.noise4d(x=nx, y=ny, z=nz, w=nw)
+
                 # height offset
-                ndy = map(n, -1, 1, -self.d_height / 4, self.d_height / 4)
+                ndy = map(n, -1, 1, -self.d_height / 2, self.d_height / 2)
 
                 dy = lines_to_draw[line]["normalized_height"]
-                line_x.append(lines_to_draw[line]["start"]["x"])
-                line_y.append(lines_to_draw[line]["start"]["y"] - dy + ndy)
+                line_x.append(lines_to_draw[line]["start"]["x"] + self.dx)
+                line_y.append(lines_to_draw[line]["start"]["y"] - dy + ndy + self.dy)
 
                 # this skips gaps (for example, sea)
-                if line < len(lines_to_draw) - 1 and lines_to_draw[line+1]["start"]["x"] - lines_to_draw[line]["end"]["x"] > self.max_gap:
+                gap = lines_to_draw[line+1]["start"]["x"] - lines_to_draw[line]["end"]["x"]
+                if line < len(lines_to_draw) - 1 and gap > self.max_gap:
                     spline_coords = spline(line_x, line_y, self.x_levels)
                     self.line_coords.append(spline_coords)
                     line_x = []
@@ -227,24 +260,34 @@ class Map:
             line_y = []
 
     def drawOutput(self):
-        self.dest_im = Image.new('RGB', (int(self.source_im.width), int(self.source_im.height)))
+        temp_im = Image.new('RGB', (int(self.source_im.width), int(self.source_im.height)))
+        draw = ImageDraw.Draw(temp_im)
 
-        draw = ImageDraw.Draw(self.dest_im)
         # reset image
-        draw.rectangle([0, 0, self.dest_im.width, self.dest_im.height], fill=(0, 0, 0))
+        draw.rectangle([0, 0, temp_im.width, temp_im.height], fill=(0, 0, 0))
 
         for line in self.line_coords:
             if not line:
                 continue
 
-            draw.line((line), fill=(255, 255, 255), width=self.line_width)
+            draw.line((line), fill=(255, 255, 255, self.line_alpha), width=self.line_width)
 
         # hide logo
         draw.rectangle([7250, 0, 10700, 950], fill=(0, 0, 0))
 
         new_width = int(self.source_im.width * self.scl)
         new_height = int(self.source_im.height * self.scl)
-        self.dest_im = self.dest_im.resize((new_width, new_height), Image.ANTIALIAS)
+        temp_im = temp_im.resize((new_width, new_height), Image.ANTIALIAS)
+
+        self.dest_im = Image.new('RGB', (self.destination_size, self.destination_size))
+        self.dest_im.paste(temp_im, (self.dx, self.dy))
+
+        # add watermark
+        draw = ImageDraw.Draw(self.dest_im)
+        text = "Lorenzo Rossi - www.lorenzoros.si"
+        x = self.font_size * 0.5
+        y = self.destination_size - self.font_size * 1.5
+        draw.text((x, y), fill=(255, 255, 255, 32), text=text, font=self.font)
 
     def saveDestImage(self, folder, filename, frame_num):
         ext = "png"
@@ -268,19 +311,29 @@ class Map:
 def main():
     # FFMPEG command:
     # ffmpeg -y -r 60 -i frames/italy_%07d.png -loop 0 output/video.mp4
+    # cd /mnt/c/lorenzo/python/perlin-italy-map
+    # python3 perlin-italy-map.py
 
     logging_file = __file__.replace(".py", ".log")
-    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO, filemode="w", filename=logging_file)
-    print(f"Logging into file{logging_file}\n\n")
+    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO)
+    print(f"Logging into file {logging_file}\n\n")
     logging.info("Script started")
     script_start = time.time()
 
     source_file = "source/Italia_tinitaly.jpg"
+    font = "source/Roboto-LightItalic.ttf"
     destination_folder = "frames"
     video_folder = "output"
     destination_file = "italy"
+    # output video fps
     fps = 60
+    # output video duration
     duration = 15
+    # size of output video
+    output_size = 120
+
+    total_frames = fps * duration
+    m = Map(output_size)
 
     logging.info("Creating folders...")
     try:
@@ -296,11 +349,13 @@ def main():
     Path(video_folder).mkdir(parents=True, exist_ok=True)
     logging.info("Folders created")
 
-    total_frames = fps * duration
-    m = Map()
     logging.info("Loading images...")
     m.loadSource(source_file)
     logging.info("Images loaded")
+
+    logging.info("Loading font ...")
+    m.loadFont(font)
+    logging.info("Font loaded")
 
     logging.info("Loading lines...")
     m.loadLines()
@@ -329,10 +384,17 @@ def main():
         m.checkPause()
         if percent > 0:
             elapsed = int(time.time() - generating_start)
+            elapsed_min = int(elapsed / 60)
             total = elapsed / percent
             remaining = int(total - elapsed)
             remaining_min = int(remaining / 60)
-            logging.info(f"Time elapsed: {elapsed}s. Remaining: {remaining}s (~{remaining_min} min).")
+
+            log_text = (
+                    f"Time elapsed: {elapsed}s (~{elapsed_min}min). "
+                    f"Remaining: {remaining}s (~{remaining_min} min)."
+            )
+
+            logging.info(log_text)
 
     # generate the output video
     options = f"ffmpeg -y -r {fps} -i {destination_folder}/{destination_file}_%07d.png -loop 0 {video_folder}/{destination_file}.mp4"
